254be20cb6f357629b3c46f176c33328fb54e521
Elannio <elanniosousa18@gmail.com>
2023-09-03 19:33:46 -0300

    Adicionei algumas alterações na home, como as categorias, os jogos favoritos e banners

--- Elannio/scripts.js
+++ Elannio/scripts.js
@@ -1,43 +1,14 @@
-// app.js
-const express = require('express');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const app = express();
+<script>
+// Associando o botão e a lista de seleção a variáveis em JavaScript
+const botaoEscolha = document.getElementById('botaoEscolha');
+const opcoes = document.getElementById('opcoes');
 
-// Configuração de middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ secret: 'your-secret-key', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+// Adicionando um ouvinte de evento ao botão para lidar com o clique
+botaoEscolha.addEventListener('click', function() {
+    // Obtendo o valor selecionado na lista de seleção
+    const opcaoSelecionada = opcoes.value;
 
-// Configuração da estratégia de autenticação local
-passport.use(new LocalStrategy((username, password, done) => {
-    // Implemente a lógica de autenticação aqui (por exemplo, consultando um banco de dados)
-    if (username === 'usuario' && password === 'senha') {
-        return done(null, { id: 1, username: 'usuario' });
-    } else {
-        return done(null, false, { message: 'Login inválido' });
-    }
-}));
-
-// Serialização e desserialização do usuário
-passport.serializeUser((user, done) => {
-    done(null, user.id);
+    // Fazendo algo com a opção selecionada, por exemplo, exibindo-a em um alerta
+    alert('Opção selecionada: ' + opcaoSelecionada);
 });
-
-passport.deserializeUser((id, done) => {
-    // Consulte o banco de dados ou sistema de armazenamento para obter informações do usuário
-    done(null, { id: 1, username: 'usuario' });
-});
-
-// Rotas
-const routes = require('./routes');
-app.use('/', routes);
-
-// Iniciar o servidor
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Servidor em execução na porta ${port}`);
-});
+</script>
