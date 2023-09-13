83490afbba425578d0283a5bc847056eb536fa85
Elannio <elanniosousa18@gmail.com>
2023-09-13 16:23:30 -0300

    Adicionei umas coisas

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Configurar rotas e controladores aqui...
+
+app.listen(port, () => {
+  console.log(`Servidor está rodando na porta ${port}`);
+});
+
