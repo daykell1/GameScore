76dc5d3202bd3fcfaaa83c6f413799e1a4ad1dc6
Elannio <elanniosousa18@gmail.com>
2023-10-01 18:01:56 -0300

    Iniciei os comandos do banco de dados para o game

--- Game/views.py
+++ Game/views.py
@@ -3,5 +3,19 @@ from .models import Game
 from django.http import HttpResponse
 
 def create(request):
-    SuperMario = Game (gamenome = 'Super Mário World', descricao = '', nota = '', Categoria = 'Adventure', foto = 'super.webp')
-    SuperMario.save()
+    try:
+        # Crie um objeto Game com os detalhes
+        super_mario = Game(gamenome='Super Mario World', descricao='Descrição do jogo', nota='', categoria='Adventure')
+        
+        # Carregue a imagem (foto) do jogo, se você a tiver no local correto e configurou corretamente o upload de arquivos.
+        with open('../Game/fotos/super.webp', 'rb') as img_file:
+            super_mario.foto.save('super.webp', File(img_file), save=True)
+
+        # Salve o objeto no banco de dados
+        super_mario.save()
+
+        return HttpResponse('Jogo criado com sucesso!')
+    except Exception as e:
+        return HttpResponse(f'Erro ao criar o jogo: {str(e)}')
+
+
