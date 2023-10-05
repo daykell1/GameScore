8b7924b0ebf2669057494548956590a1de6f22b4
Dayane kelly <54118755+daykell1@users.noreply.github.com>
2023-10-05 18:15:19 -0300

    subindo arquivos

--- CreateAccount/views.py
+++ CreateAccount/views.py
@@ -14,26 +14,3 @@ def create(request):
         return HttpResponse("Cadastro feito com sucesso!")
     else:
         return render(request,"telaDeCriarConta.html")
-    
-def dashboard(request):
-    # Lógica para renderizar a página de destino após o login
-    return render(request, '../html/index.html')
-    
-    
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('dashboard')  # Redirecione para a página de dashboard
-        else:
-            # Exibir mensagem de erro de login
-            return render(request, 'telaDeLogin.html')
-    else:
-        return render(request, 'telaDeLogin.html')
-        
-
-
-
