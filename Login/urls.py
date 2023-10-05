fc186f49f31dbacb62ec224ed28c9caa19436fbf
Elannio <elanniosousa18@gmail.com>
2023-10-05 18:55:59 -0300

    Alterei algumas partes do login

--- Login/urls.py
+++ Login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('',views.login_view,name='login_view'),
+    path('', views.login_view, name='login_view'),
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- Login/views.py
+++ Login/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+import logging
+
+logger = logging.getLogger(__name__)
 
 def dashboard(request):
-    # Lógica para renderizar a página de destino após o login
-    return render(request, 'html/index.html')  # Certifique-se de que o caminho do seu template está correto
+    logger.debug('Página de dashboard acessada.')
+    return render(request, 'html/index.html')
 
 def login_view(request):
     if request.method == 'POST':
@@ -12,11 +15,10 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            print(f'Usuário {username} autenticado com sucesso.')
-            return redirect('dashboard')  # Redirecione para a página de dashboard
+            logger.debug(f'Usuário {username} autenticado com sucesso.')
+            return redirect('dashboard')
         else:
-            # Exibir mensagem de erro de login
-            print(f'Falha na autenticação do usuário {username}.')
+            logger.debug(f'Falha na autenticação do usuário {username}.')
             return render(request, 'telaDeLogin.html')
     else:
         return render(request, 'telaDeLogin.html')
